6ff33380a6b9133c208ac0bec4def406aa90712a
Krish <msk972645@gmail.com>
2025-09-24 19:32:57 +0530

    vsc2.sh

---
+++ vsc2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#match convo btw rohith and kohli
+echo "kohli::hi rohith"
+echo "rohith::hi kohli, how is your practice for tomorrow match"
+echo "kohli::i am super exicted, bcz its ind vs pak match"
+echo "rohith::hmm, you like ind vs pak match always"
+echo "kohli::yes rohith, you know already"
+echo "rohith::ye i know"
