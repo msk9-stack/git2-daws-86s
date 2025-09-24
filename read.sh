0c00856e8473c0e2fbe5da0182ddf4f9302fc2e3
Krish <msk972645@gmail.com>
2025-09-24 22:17:21 +0530

    door lock number

--- read.sh
+++ read.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-#the conversation btw amit and susheel, after a long time the were met
+#the conversation btw P1 and P2, after a long time the were met
 
 P1=$1
 P2=$2
 
-echo "Amit: Hey Susheel! Long time no see."
-echo "Susheel: Amit! Wow, it’s been ages."
-echo "Amit: How have you been?"
-echo "Susheel: Doing well, busy with work. You?"
-echo "Amit: Same here, work and family keep me occupied."
-echo "Susheel: Do you remember our college trips?"
-echo "Amit: Of course! Those were some of the best days."
-echo "Susheel: We should plan a reunion soon."
-echo "Amit: Definitely, let’s make it happen this year."
-echo "Susheel: Great! I’ll start checking with others."
-echo "Amit: Shusheel, can you give your current phone number?"
+echo "P1: Hey P2! Long time no see."
+echo "P2: P1! Wow, it’s been ages."
+echo "P1: How have you been?"
+echo "P2: Doing well, busy with work. You?"
+echo "P1: Same here, work and family keep me occupied."
+echo "P2: Do you remember our college trips?"
+echo "P1: Of course! Those were some of the best days."
+echo "P2: We should plan a reunion soon."
+echo "P1: Definitely, let’s make it happen this year."
+echo "P2: Great! I’ll start checking with others."
+echo "P1: P2, can you give your current phone number?"
 
-read -s Susheel_PHONE_NUMBER 
+read -s P2_PHONE_NUMBER 
 
-echo "Shusheel here is my phone number: $Susheel_PHONE_NUMBER"
+echo "P2 here is my phone number: $P2_PHONE_NUMBER"
 
-echo "Susheel: Amit, can you also provide your current phone number?"
+echo "P2: P1, can you also provide your current phone number?"
 
-read -s Amit_PHONE_NUMBER
+read -s P1_PHONE_NUMBER
 
-echo "Amit: Sure Susheel, here is my current phone number $Amit_PHONE_NUMBER" 
+echo "P1: Sure P2, here is my current phone number $P1_PHONE_NUMBER" 
 
-echo "Amit: okay bye Susheel"
-echo "Susheel: Bye Amit, See you soon"
+echo "P1: okay bye P2"
+echo "P2: Bye P1, See you soon"
