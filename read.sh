698fc15a8d1decfafbbf41a7c5ee9bcbba188378
Krish <msk972645@gmail.com>
2025-09-24 21:50:49 +0530

    door lock number

--- read.sh
+++ read.sh
@@ -16,6 +16,6 @@ echo "Susheel: We should plan a reunion soon."
 echo "Amit: Definitely, let’s make it happen this year."
 echo "Susheel: Great! I’ll start checking with others."
 
-read -s Amit:canyou give your updated PHONE_NUMBER #phone number
+read -s "Amit:canyou give your updated PHONE_NUMBER" #phone number
 
 echo "Shusheel here is my $PHONE_NUMBER"
