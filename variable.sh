a883b406c5d60e9a52b598f656316a2331cc27c2
Krish <msk972645@gmail.com>
2025-09-24 19:59:47 +0530

    variable.sh

---
+++ variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PERSON1=dhoni
+PERSON2=rahul
+
+#match convo btw rohith and kohli
+
+echo "$PERSON1::hi $PERSON2"
+echo "$PERSON2::hi $PERSON1, how is your practice for tomorrow match"
+echo "$PERSON1::i am super exicted, bcz its ind vs pak match"
+echo "$PERSON2::hmm, you like ind vs pak match always"
+echo "$PERSON1::yes $PERSON2, you know already"
+echo "$PERSON2::ye i know"
